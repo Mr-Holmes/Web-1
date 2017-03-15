c98082eb41c98bdcb82e12d501c2a95d3d432c2e
Mr-Holmes <mugiwara.gl@gmail.com>
2017-03-14 21:22:44 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fomulario</title>
+</head>
+<body>
+ 	<form action="" method="POST">
+ 		<fieldset>
+ 			<legend>Insira seus dados</legend>
+ 				<br>
+ 				Nome:<br>
+ 					<input type="text" name="Nome">
+ 				Idade:<br>
+ 					<input type="text" name="Idade">
+ 		</fieldset>
+ 		
+ 	</form>
+</body>
+</html>
